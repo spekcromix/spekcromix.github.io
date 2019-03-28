85389025282270eab725b39df26fe9db51827689
rodolfole <rodolfolarae@gmail.com>
2019-03-27 19:49:32 -0600

    Primer commit

---
+++ precache-manifest.95036bb72bb0519ab70d7ed1a675afc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9490f232bd11696904ee",
+    "url": "/static/css/main.7ed6851e.chunk.css"
+  },
+  {
+    "revision": "9490f232bd11696904ee",
+    "url": "/static/js/main.ed0fcc88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "0f2f6d8724a5fae1e1ceda8fcb9ce8ab",
+    "url": "/index.html"
+  }
+];
